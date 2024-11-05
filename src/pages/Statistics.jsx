457e96955f2287ec7a569d6c778890f6f946e39b
jubayer <jubayerahmed46@gmail.com>
2024-11-05 17:25:30 +0600

    title updata

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function Statistics() {
   useEffect(() => {
-    document.title = "Home || Gadget Heaven";
+    document.title = "Statistics || Gadget Heaven";
   }, []);
   return <div>statistics</div>;
 }
